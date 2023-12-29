15b4d8610058db460c46ed28ba37066d56650d24
Ryan England <me@rengland.org>
2023-12-29 14:08:58 -0800

    Added fade text animation. needs debugging

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -57,9 +57,44 @@ const Home = () => {
         }, 2000);
     }
 
+    const fadeText = () => {
+        const titleElement = document.querySelector('.pftitle');    // Find title element
+        let i = 0;                                                  // Set initial opacity to 0
+        setDisplayText(name);                                       // Set display text to name
+
+        const gradientDirection = Math.floor(Math.random() * 360);  // Random gradient direction
+
+        // Fade in title
+        const fadeInterval = setInterval(() => {
+            let gradient = `linear-gradient(
+                ${gradientDirection}deg, 
+                rgba(255,255,255,1) ${ i > 100 ? i-100 : 0}%, 
+                rgba(0,0,0,1) ${i > 100 ? 100 : i}%
+            )`
+
+            titleElement.style.color = gradient;
+            i++;
+        }, 50);
+
+        // Clear timeout after i=100% opacity for both elements, set color to white
+        setTimeout(() => {
+            clearInterval(scrambleInterval);
+            titleElement.style.color = "#FFF";
+        }, 100000);
+    }
+
     // run shuffleText() on page load
     useEffect(() => {
-        shuffleText();
+        const rand = Math.floor(Math.random() * 2);
+
+        switch (rand) {
+            case 0:
+                shuffleText();
+                break;
+            case 1:
+                fadeText();
+                break;
+        }
     }, []);
 
     return (
